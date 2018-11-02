227d5c7b7a5ad9e1be82d507b388bcd97ed560aa
yousrahmed <yousrahmed46@yahoo.com>
2018-11-02 23:28:02 +0200

    Initial commit

---
+++ my task/Controllers/HIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_task.Controllers
+{
+    public class HIController : Controller
+    { //
+        //GET:HI/
+        public string Index()
+        {
+            return "This iS MY Default Action..xD";
+        }
+       
+            
+    }
+ 
+    }
---
+++ my task/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace my_task.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
